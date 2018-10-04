6fb70c768dbcd1a522f450d4f59783228e422d56
liangyling3 <liangyling3@163.com>
2018-10-04 19:11:15 +0800

    修改

--- 火车轨道调度.cpp
+++ 火车轨道调度.cpp
@@ -6,55 +6,31 @@
 #include <iomanip>
 using namespace std;
 
-vector<int> SortByStack(vector<int> unsortedNumbers) {
+vector<int> getArbitraryNumber (int size) {
+	vector<int> arbitraryNumbers;
+	srand((int)time(NULL));
+	int * arr = new int [size];
+	//	generate non-repeating arbitrary numbers by swaping
+	for (int i = 0; i < size; ++ i)		arr[i]=i+1;
+	for (int i = size-1; i >= 0; -- i)	swap(arr[i], arr[rand()%(i+1)]);
+	for (int i = 0; i < size; ++ i)		arbitraryNumbers.push_back(arr[i]);
+	delete [] arr;
+	return arbitraryNumbers;
+}
+
+
+void sortByStack(vector<int> &unsortedNumbers) {
 	vector<int> sortedNumbers;
 	vector<stack<int> > instack;
 	
 	while (unsortedNumbers.size() != 0) {
-		//	unsortedNumbers to sortedNumbers
-		int flag = 0;
-		if (sortedNumbers.size() == 0 && unsortedNumbers[0] == 1) {
-			sortedNumbers.push_back(1);
-			unsortedNumbers.erase(unsortedNumbers.begin());
-			flag = 1;
-		}	 
-		else if (sortedNumbers.size() != 0 && (unsortedNumbers[0] == sortedNumbers[sortedNumbers.size()-1] + 1)) {
-			sortedNumbers.push_back(unsortedNumbers[0]);
-			unsortedNumbers.erase(unsortedNumbers.begin());
-			flag = 1;
+		int isSorted = unsortedToSorted(unsortedNumbers, sortedNumbers);
+		while (isSorted == 1) {
+			int count = stackToSorted(instack, sortedNumbers);
+			if(count == 0) break;
 		}
-		
-		//	stack to sortedNumbers
-		while (flag == 1) {
-			int count = 0;
-			for (int i = 0; i < instack.size(); ++ i) {
-				if (instack[i].top() == (sortedNumbers[sortedNumbers.size()-1]+1)) {
-					sortedNumbers.push_back(instack[i].top());
-					instack[i].pop(); 
-					count ++;
-				}
-				if (instack[i].empty())	instack.erase(instack.begin()+i);
-			}
-			if (count == 0)	break;
-		}
-		
-		//	unsortedNumbers to stack
-		if (flag == 0) {
-			int count = 0;
-			for (int i = 0; i < instack.size(); ++ i) {
-				if (unsortedNumbers[0] < instack[i].top()) {
-					instack[i].push(unsortedNumbers[0]);
-					unsortedNumbers.erase(unsortedNumbers.begin());
-					count = 1;
-					break;
-				}
-			}	
-			if (count == 0) {
-				stack<int> s;
-				instack.push_back(s);
-				instack[instack.size()-1].push(unsortedNumbers[0]);
-				unsortedNumbers.erase(unsortedNumbers.begin());
-			}
+		if (isSorted == 0) {
+			unsortedToStack(unsortedNumbers, instack);
 		}
 	}
 	
@@ -68,20 +44,56 @@ vector<int> SortByStack(vector<int> unsortedNumbers) {
 		}
 	}
 	
-	return sortedNumbers;
+	unsortedNumbers = sortedNumbers;
 }
 
-vector<int> getArbitraryNumber (int size) {
-	vector<int> arbitraryNumbers;
-	srand((int)time(NULL));
-	int * arr = new int [size];
-	//	generate non-repeating arbitrary numbers by swaping
-	for (int i = 0; i < size; ++ i)		arr[i]=i+1;
-	for (int i = size-1; i >= 0; -- i) 	swap(arr[i], arr[rand()%(i+1)]);
-	for (int i = 0; i < size; ++ i)		arbitraryNumbers.push_back(arr[i]);
-	delete [] arr;
-	return arbitraryNumbers;
+int unsortedToSorted(vector<int> &unsortedNumbers, vector<int> &sortedNumbers) {
+	int flag = 0;
+	//	the first element
+	if (sortedNumbers.size() == 0 && unsortedNumbers[0] == 1) {
+		sortedNumbers.push_back(1);
+		unsortedNumbers.erase(unsortedNumbers.begin());
+		flag = 1;
+	}	 
+	else if (sortedNumbers.size() != 0 && (unsortedNumbers[0] == sortedNumbers[sortedNumbers.size()-1] + 1)) {
+		sortedNumbers.push_back(unsortedNumbers[0]);
+		unsortedNumbers.erase(unsortedNumbers.begin());
+		flag = 1;
+	}
+	return flag;
+} 
+
+int stackToSorted(vector<stack<int> > &instack, vector<int> &sortedNumbers) {
+	int count = 0;
+	for (int i = 0; i < instack.size(); ++ i) {
+		if (instack[i].top() == (sortedNumbers[sortedNumbers.size()-1]+1)) {
+			sortedNumbers.push_back(instack[i].top());
+			instack[i].pop(); 
+			count ++;
+		}
+		if (instack[i].empty())	instack.erase(instack.begin()+i);
+	}
+	return count;
 }
+
+void unsortedToStack(vector<int> &unsortedNumbers, vector<stack<int> > &instack) {
+	int count = 0;
+	for (int i = 0; i < instack.size(); ++ i) {
+		if (unsortedNumbers[0] < instack[i].top()) {
+			instack[i].push(unsortedNumbers[0]);
+			unsortedNumbers.erase(unsortedNumbers.begin());
+			count = 1;
+			break;
+		}
+	}	
+	if (count == 0) {
+		stack<int> s;
+		instack.push_back(s);
+		instack[instack.size()-1].push(unsortedNumbers[0]);
+		unsortedNumbers.erase(unsortedNumbers.begin());
+	}
+}
+
 
 int main() {
 	cout << "Please enter the size of the train: " << endl;  
@@ -98,10 +110,11 @@ int main() {
 	cout << endl << endl; 
 	
 	cout << "The numbers sorted are :" << endl;
-	vector<int> sortedNumbers = SortByStack(trainNumbers);
+	sortByStack(trainNumbers);
 	for (int i = 0; i < size; ++ i) {
-		cout << setw(5) << sortedNumbers[i];
+		cout << setw(5) << trainNumbers[i];
 		if ((i+1)%15==0)	cout << endl;
 	}
 	cout << endl;
+	
 }
